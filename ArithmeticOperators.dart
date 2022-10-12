9b988c31aa21c324c7ee1e1e9ba2a1a5b46bfd63
aladin <msbih.khalil@gmail.com>
2022-10-12 20:05:46 +0100

    add ArithmeticOperators

---
+++ ArithmeticOperators.dart
@@ -0,0 +1,25 @@
+void main() {
+  double a = 1;
+  double b = 5;
+
+  // addition
+
+  double result = a + b;
+  print("$a + $b = $result");
+
+  // substraction
+
+  result = a - b;
+
+  print("$a - $b = $result");
+
+  // multiply
+  result = a * b;
+  print("$a * $b = $result");
+
+  // division
+  result = a / b;
+
+  print("$a / $b = $result");
+  
+}
