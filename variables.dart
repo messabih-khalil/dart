6e1c49cf2d48df6a432f259865063d7b224d790c
aladin <msbih.khalil@gmail.com>
2022-10-12 19:56:19 +0100

    add variables

---
+++ variables.dart
@@ -0,0 +1,39 @@
+void main() {
+  // string
+
+  String name = "aladinn";
+
+  print("my name is : $name");
+
+  // integer
+
+  int age = 20;
+
+  print("my age : $age");
+
+  // double
+
+  double pi = 3.14;
+
+  print("pi : $pi");
+
+  // boolean
+  bool active = true;
+
+  print("is active : $active");
+
+  // Null Safety
+
+  int? unknown;
+  print(unknown);
+  unknown = 1;
+  print(unknown);
+
+  // object (Map)
+
+  // list
+
+  // tuples
+
+  // sets
+}
